f74e3f08013cb7aad88dfc065a2a93f5bcf675fa
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-13 18:26:46 +0300

    Create char_as_int.c

---
+++ char_as_int.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char v = 89;
+    char f = 'a';
+    printf("v as integer: %d\n", v);
+    printf("v character: %c\n", v);
+    printf("f as integer: %d\n", f);
+    printf("f as character: %c\n", f);
+
+    return 0;
+}
+
+
+
